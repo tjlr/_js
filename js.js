df2371f961379ba71a44e687f4152bb5f0e9617b
tjlr <tomek.rybarczyk@gmail.com>
2016-02-11 15:40:41 +0100

    Update js.js
    fix _e

--- js.js
+++ js.js
@@ -5,5 +5,5 @@ function _cr(el) {return document.createElement(''+el+'');}
 function _ap(el,dest) {_q(''+dest+'').appendChild(el);}
 function _pp(el,dest) {_q(''+dest+'').insertBefore(el,_q(''+dest+'').firstChild);}
 function _i(el,str) {el.innerHTML=str;}
-function _e(el) {$(el).innerHTML='';}
+function _e(el) {el.innerHTML='';}
 function escapeHtml(s) { s=s.replace(/&/g,'&amp;'); s=s.replace(/</g,'&lt;'); s=s.replace(/>/g,'&gt;'); s=s.replace(/"/g,'&quot;'); s=s.replace(/'/g,'&#39;'); return s}
