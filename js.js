51660dcc6e5e0f8fbc3834831509b69ef5ba0f19
tjlr <tomek.rybarczyk@gmail.com>
2016-02-15 23:34:59 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,16 +3,10 @@ function _q(el) {return document.querySelector(el);}
 function _a(el) {return document.querySelectorAll(el);}
 function _cr(el) {return document.createElement(''+el+'');}
 function _rm(el) {el.parentNode.removeChild(el);}
-function _ap(el,dest) {_q(''+dest+'').appendChild(el);}
-function _pp(el,dest) {_q(''+dest+'').insertBefore(el,_q(''+dest+'').firstChild);}
-function _i(el,str) {el.innerHTML=str;}
+function _ap(el,dest) {dest.appendChild(el);}
+function _pp(el,dest) {dest.insertBefore(el,dest.firstChild);}
+function _i(el,html) {el.innerHTML=html;}
 function _e(el) {el.innerHTML='';}
 function escapeHtml(s) { s=s.replace(/&/g,'&amp;'); s=s.replace(/</g,'&lt;'); s=s.replace(/>/g,'&gt;'); s=s.replace(/"/g,'&quot;'); s=s.replace(/'/g,'&#39;'); return s}
-
-var js = new function() {
-	this.q = function(el) {return document.querySelector(el);}
-	this.cr = function(el) {return document.createElement(''+el+'');}
-}
-
-//example
-js.cr('div');
+function notice(msg) { var nt=_cr('div'); _i(nt,msg); nt.className='notice'; _pp(nt,document.body); }
+function clean() { [].forEach.call(_a('.notice'),function(e){_rm(e);}); }
