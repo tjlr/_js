66db9a28b2e6cd35d58434881f893f41580aa4d1
tjlr <tomek.rybarczyk@gmail.com>
2016-02-25 15:06:43 +0100

    _x ajax request

--- js.js
+++ js.js
@@ -8,6 +8,7 @@ function _pp(el,dest){dest.insertBefore(el,dest.firstChild);}
 function _i(el,html){el.innerHTML=html;}
 function _e(el){el.innerHTML='';}
 function _sa(el,at,v){el.setAttribute(at,v);}
+function _x(u,c) {var x=new XMLHttpRequest();x.open("GET",u,true);x.onreadystatechange=function(){if(x.readyState==4){c.call(x.responseText);}};x.send();}
 function escapeHtml(s) { s=s.replace(/&/g,'&amp;'); s=s.replace(/</g,'&lt;'); s=s.replace(/>/g,'&gt;'); s=s.replace(/"/g,'&quot;'); s=s.replace(/'/g,'&#39;'); return s}
 function notice(msg) { var nt=_cr('div'); _i(nt,msg); nt.className='notice'; _pp(nt,document.body); }
 function clean() { [].forEach.call(_a('.notice'),function(e){_rm(e);}); }
